eec019c7096017802b5918b499cd83ba9396c8f9
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-28 11:27:44 +0200

    mobile-icons-generator version v0.0.6

--- mobile-icons-generator.rb
+++ mobile-icons-generator.rb
@@ -1,12 +1,12 @@
 class MobileIconsGenerator < Formula
   desc ""
   homepage ""
-  url "https://github.com/beplus/mobile-icons-generator/releases/download/v0.0.5/mobile-icons-generator_0.0.5_macOS_64-bit.tar.gz"
-  version "0.0.5"
-  sha256 "ad21be7873db10e9aab635b25934295bca979c0e98d1713247a2dbbfb0b692c9"
+  url "https://github.com/beplus/mobile-icons-generator/releases/download/v0.0.6/mobile-icons-generator_0.0.6_macOS_64-bit.tar.gz"
+  version "0.0.6"
+  sha256 "f4bac7a996b3c70977984b206bdb6260523420e86e520e51c8a7b682cdc6e07c"
 
   def install
-    bin.install "mobile-icons-generator"
+    bin.install "makeicon"
   end
 
   test do
