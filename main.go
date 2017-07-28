ad35a7f3bef54b490138b6a487ac24b06127127e
Jan Misek <jan.misek1@gmail.com>
2017-07-28 15:57:03 +0200

    main re-style

--- main.go
+++ main.go
@@ -20,38 +20,42 @@ var args struct {
 }
 
 func main() {
-
 	_, err := flags.ParseArgs(&args, os.Args)
 	if err == nil {
 		if args.Version {
-			fmt.Printf(" Version: %v \n License: MIT \n Copyright (c) 2017 Be Bplus s.r.o. ", version)
-		}
-
-		if args.Filename == "" {
-			log.Fatal("No file set use -n or --file to set it")
-		}
-
-		// todo windows \
-		s := strings.Split(args.Filename, "/")
-		filename := s[len(s)-1]
-
-		// prepare json body
-		filenameArray := strings.Split(filename, ".")
-		name, extension := filenameArray[0], filenameArray[1]
-
-		myImage, err := image_helper.NewMyImageFromBase64(getImageBase64(args.Filename), name, extension)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Processing icons... It could few seconds...")
-
-		_, err = myImage.Upload()
-		if err != nil {
-			log.Println(err)
+			fmt.Printf(" Version: %v \n License: MIT \n Copyright (c) 2017 Be Bplus s.r.o. \n", version)
 		} else {
-			fmt.Println("Icons saved to folder 'AppIcon'.")
+
+			if args.Filename == "" {
+				log.Fatal("No file set use -n or --file to set it")
+			}
+
+			save(args.Filename)
 		}
+	}
+}
+
+func save(file string) {
+	// todo windows \
+	s := strings.Split(file, "/")
+	filename := s[len(s)-1]
+
+	// prepare json body
+	filenameArray := strings.Split(filename, ".")
+	name, extension := filenameArray[0], filenameArray[1]
+
+	myImage, err := image_helper.NewMyImageFromBase64(getImageBase64(args.Filename), name, extension)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Processing icons... It could few seconds...")
+
+	_, err = myImage.Upload()
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println("Icons saved to folder 'AppIcon'.")
 	}
 }
 
