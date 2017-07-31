4f09c0614a91dde255d26156f5efb7ee665f1f6f
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 11:42:26 +0200

    makeicon version v0.0.21

--- makeicon.rb
+++ makeicon.rb
@@ -1,9 +1,9 @@
 class Makeicon < Formula
   desc ""
   homepage ""
-  url "https://github.com/beplus/makeicon/releases/download/v0.0.20/makeicon_0.0.20_macOS_64-bit.tar.gz"
-  version "0.0.20"
-  sha256 "f9119ab690ecb95982bd41f3047cd7bd68df5c9e0e7eaaff1935d18968c0769b"
+  url "https://github.com/beplus/makeicon/releases/download/v0.0.21/makeicon_0.0.21_macOS_64-bit.tar.gz"
+  version "0.0.21"
+  sha256 "26d5c008d8433faee0d600cd80076b968697369cf9836e0adab9b882e526c49f"
 
   def install
     bin.install "makeicon"
