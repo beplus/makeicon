da72c54baa40a9f244d1d6c02168b5293b9e5b26
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 12:07:22 +0200

    makeicon version v0.0.23

--- makeicon.rb
+++ makeicon.rb
@@ -1,9 +1,9 @@
 class Makeicon < Formula
   desc ""
   homepage ""
-  url "https://github.com/beplus/makeicon/releases/download/v0.0.22/makeicon_0.0.22_darwin_amd64.tar.gz"
-  version "0.0.22"
-  sha256 "eea3c6213a3c9a2b0739bb8bf3db77464d2f9803c3f98be63d35f7478a27d23a"
+  url "https://github.com/beplus/makeicon/releases/download/v0.0.23/makeicon_0.0.23_darwin_amd64.tar.gz"
+  version "0.0.23"
+  sha256 "068126bf40a27c9b2f04c0c9f01dd0422b53339d4d0064810d7c22bb2ed22e37"
 
   def install
     bin.install "makeicon"
