212f780948b353e795c1431935866f833cf39fad
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 12:57:23 +0200

    makeicon version v0.0.27

--- makeicon.rb
+++ makeicon.rb
@@ -1,9 +1,9 @@
 class Makeicon < Formula
   desc "Generate mobile app icons in all resolutions for both iOS and Android"
   homepage "https://github.com/beplus/makeicon"
-  url "https://github.com/beplus/makeicon/releases/download/v0.0.26/makeicon_0.0.26_darwin_amd64.tar.gz"
-  version "0.0.26"
-  sha256 "a39c5d7d68b86e2d4454677af3487292a30ed3edf5fd883fac7c3b50255fe47e"
+  url "https://github.com/beplus/makeicon/releases/download/v0.0.27/makeicon_0.0.27_darwin_amd64.tar.gz"
+  version "0.0.27"
+  sha256 "c929c24d5a9ada71e45361db483ed0daacdd3e559ac17103a599881e8bc36dea"
 
   def install
     bin.install "makeicon"
