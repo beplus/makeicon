0ee39b1782dd4b6922ad3782202ddabf52e5924e
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 12:00:06 +0200

    makeicon version v0.0.22

--- makeicon.rb
+++ makeicon.rb
@@ -1,33 +1,15 @@
 class Makeicon < Formula
-  desc "Tool for generating icons in all resolutions for both iOS and Android applications"
-  homepage "https://github.com/beplus/makeicon"
-  url "https://github.com/beplus/makeicon.git",
-      :tag => "v0.0.21",
-      :revision => "8bf480cd227b69e0cccd6758f26eb0e0422aed8a"
-  sha256 "26d5c008d8433faee0d600cd80076b968697369cf9836e0adab9b882e526c49f"
-
-  head "https://github.com/beplus/makeicon.git",
-       :shallow => false
-
-  depends_on "go" => :build
-  depends_on "govendor" => :build
+  desc ""
+  homepage ""
+  url "https://github.com/beplus/makeicon/releases/download/v0.0.22/makeicon_0.0.22_darwin_amd64.tar.gz"
+  version "0.0.22"
+  sha256 "eea3c6213a3c9a2b0739bb8bf3db77464d2f9803c3f98be63d35f7478a27d23a"
 
   def install
-    contents = Dir["{*,.git,.gitignore}"]
-    gopath = buildpath/"gopath"
-    (gopath/"src/github.com/beplus/makeicon").install contents
-
-    ENV["GOPATH"] = gopath
-    ENV.prepend_create_path "PATH", gopath/"bin"
-
-    cd gopath/"src/github.com/beplus/makeicon" do
-      system "govendor", "sync"
-      system "go", "build", "-o", "bin/makeicon"
-      bin.install "bin/makeicon"
-    end
+    bin.install "makeicon"
   end
 
   test do
-    system "#{bin}/makeicon", "--version"
+    
   end
 end
